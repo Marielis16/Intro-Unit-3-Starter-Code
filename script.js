e1ae81fe9e87cf50803f76fe00c886a56450fa4f
MARIELIS ACOSTA <>
2023-03-02 14:32:36 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,30 @@
+let storyopening = document.queryselector(".story-opening");
+let optiononescreen = document.queryselector(".option-one-screen");
+let optiontwoscreen = document.queryselector("option-two-screen");
+let optiononeend = document.queryselector(".option-one-end");
+let optiontwoend = document.queryselector(".option-two-end");
+let optionone = docments.queryselector(".option-one");
+let optiontwo = docments.queryselector(".option-two");
+
+
+
+
+
+
+
+
+/* When you're ready to make event handlers, uncomment the code below. Then fill in the blanks with the correct variables!
+INSERT_VARIABLE.onclick=function(){
+
+};
+
+INSERT_VARIABLE.onclick=function(){
+
+};
+
+
+INSERT_VARIABLE.onclick=function(){
+
+};
+
+*/
